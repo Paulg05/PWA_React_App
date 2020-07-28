ad435c880c08e2fe3ed65bfc4c4f908eb568d566
Paul Gonzalez <robertgonzalez018@gmail.com>
2020-07-28 16:16:46 -0700

    added fetchWeather logic to app component to render city and country to the browser

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import './App.css'
-import fetchWeather from './api/fetchWeather';
+import React, { useState } from "react";
+import "./App.css";
+import fetchWeather from "./api/fetchWeather";
 
 const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeater] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      console.log(data);
+
+      setWeater(data);
+      setQuery("");
+    }
+  };
+
   return (
     <div className="main-container">
-      <input type="text "/>
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+        </div>
+      )}
     </div>
-  )
-}
-
-
+  );
+};
 
 export default App;
