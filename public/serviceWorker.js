d46c402efe76a97ae93770e0b83e1f3729b848c5
Paul Gonzalez <robertgonzalez018@gmail.com>
2020-07-28 18:00:32 -0700

    finished setting up the SW to install, fetch, and activate

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,46 @@
 //  CACHE STAND FOR THE STORAGE OF THE BROSWER
-const CACHE_NAME = 'version-1';
-const urlsToCache = ['index.html', 'offline.html'];
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
 
+// this in the sw files actuall represents the SW
+const self = this;
+
+// install SW
+self.addEventListener("install", (event) => {
+  //were going to open the cache and add the files to the cache
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+//listen for requests
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
